2de99c849eeb3a6223dd23302ca5527724d10f80
unknown <limteikyee@gmail.com>
2019-04-07 19:28:30 +0800

    install socket.io

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
 import express from 'express'
 import Binance from 'node-binance-api'
+import socket from 'socket.io'
 import cors from 'cors'
 
-const binance = Binance()
-
 const app = express()
-
+const binance = Binance()
+const data = {
+	binance: {
+		bnb_markets: {},
+		btc_markets: {},
+		eth_markets: {},
+		usd_markets: {},
+	},
+}
 app.use(cors())
+
+const server = app.listen(3000, () =>
+	// eslint-disable-next-line no-console
+	console.log('app listening on port 3000!')
+)
+
+const io = socket(server)
 
 app.get('/', (req, res) => {
 	binance.prevDay(false, (error, prevDay) => {
 		res.send(JSON.stringify(prevDay))
 	})
 })
+binance.websockets.prevDay(false, (error, res) => {
+	const markets = res.symbol.substring(3)
+	if (markets.lastIndexOf('BTC') !== -1) {
+		data.binance.btc_markets[res.symbol] = res
+	} else if (markets.lastIndexOf('BNB') !== -1) {
+		data.binance.bnb_markets[res.symbol] = res
+	} else if (markets.lastIndexOf('ETH') !== -1) {
+		data.binance.eth_markets[res.symbol] = res
+	} else {
+		data.binance.usd_markets[res.symbol] = res
+	}
+})
 
-// eslint-disable-next-line no-console
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+setInterval(() => {
+	io.emit('data', data)
+}, 5000)
